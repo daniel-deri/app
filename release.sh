80bef1f9dbffa7bea2bc176882345459f8b07da7
huangwenchao2000 <wenchao.huang@dfactory.tech>
2021-06-05 21:45:33 +0800

    release

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ cp index.html index.bak.html
 cp asset-manifest.json asset-manifest.bak.json
 cp manifest.json manifest.bak.json
 source ~/.bashrc
-yarn --nomap --cwd /Users/lihaidong/work/app.deri.finance build 
+yarn --nomap --cwd ~/work/app.deri.finance build 
 cp -R /Users/lihaidong/work/app.deri.finance/build/* ./
 git checkout main
 git pull origin main
